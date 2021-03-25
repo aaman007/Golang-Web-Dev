77d01c921d39cacdf1d36cfea2bca8a13e681a1a
Amanur Rahman <aaman.sunny007@gmail.com>
2021-03-26 00:35:14 +0600

    net/http practice

---
+++ nethttp/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type handler int
+
+func (h handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	res.Header().Set("GoogleAPIKey", "HS&*S*GSHGSSJHGS&^%S&%SFVSSGFS")
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	data := struct {
+		Method string
+		Submissions url.Values
+		URL *url.URL
+		Header http.Header
+		ContentLength int64
+	}{
+		req.Method,
+		req.Form,
+		req.URL,
+		req.Header,
+		req.ContentLength,
+	}
+
+	tpl.ExecuteTemplate(res, "index.html", data)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.html"))
+}
+
+func main() {
+	var h handler
+	http.ListenAndServe(":8000", h)
+}
